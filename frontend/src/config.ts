09babfd62c1280d946520e3df36a7193e6f40e4d
Camilo Garces Sepulveda <camilo@Camilos-MacBook-Pro.local>
2026-09-08 16:28:38 -0500

    add config vite types

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -10,4 +10,8 @@ export const config = {
   apiUrl: need('VITE_API_URL', import.meta.env.VITE_API_URL),
   region: need('VITE_AWS_REGION', import.meta.env.VITE_AWS_REGION),
   clientId: need('VITE_COGNITO_CLIENT_ID', import.meta.env.VITE_COGNITO_CLIENT_ID),
+  userPoolId: need(
+    'VITE_COGNITO_USER_POOL_ID',
+    import.meta.env.VITE_COGNITO_USER_POOL_ID
+  ),
 };
